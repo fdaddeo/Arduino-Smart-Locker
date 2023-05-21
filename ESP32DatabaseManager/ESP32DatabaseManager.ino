0df0d8d767a7c7ca90704046299bcbd1105a64f8
mdaddeo <nicdaddy@libero.it>
2023-05-21 19:39:01 +0200

    Added ESP32

---
+++ ESP32DatabaseManager/ESP32DatabaseManager.ino
@@ -0,0 +1,38 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+#define LED 2
+
+// Enter network info
+const char* ssid = "";
+const char* password = "";
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH); // Spegni il led
+
+  IPAddress ip(192, 168, 1, 200);
+  IPAddress gateway(192, 168, 1, 1);
+  IPAddress subnet(255, 255, 255, 0);
+  IPAddress dns1(8, 8, 8, 8); //Dns1
+  IPAddress dns2(8, 8, 4, 4); //Dns
+
+  WiFi.config(ip, gateway, subnet, dns1, dns2); //per indirizzo statico
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  
+  Serial.println();
+  Serial.println("Started");
+}
+
+void loop() {
+
+}
